38758f64331e90d5c65ffc653048c01320e362bd
PhDuy2005 <kduy1706@gmail.com>
2026-06-15 09:52:45 +0700

    fix env

--- run.py
+++ run.py
@@ -1,5 +1,12 @@
+import os
+
 import uvicorn
 
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8082, reload=True)
+    uvicorn.run(
+        "app.main:app",
+        host=os.getenv("APP_HOST", "127.0.0.1"),
+        port=int(os.getenv("APP_PORT", "8082")),
+        reload=True,
+    )
